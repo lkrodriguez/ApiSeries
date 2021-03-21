be821b0d08de676a4c818f8711f53609453c76b2
lkrodriguez <41454865+lkrodriguez@users.noreply.github.com>
2021-03-20 23:04:52 -0500

    Index
    se llaman datos ,mas no img

--- view/all/header.php
+++ view/all/header.php
@@ -1,22 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-
-    <title>SERIES |</title>
+    <meta charset="utf-8">
+    <title>Series & Peliculas</title>
     <link rel="stylesheet" link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" >
     <link rel="shortcut icon" href="http://tostaocafeypan.com/wp-content/uploads/2016/09/favicon.png"/>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+   
 
 </head>
 
-
-
-
-
 <body  style="overflow-x: hidden;" >
-
-
-
-
 
 <style type="text/css">
     .con{
@@ -33,14 +29,30 @@
         border-radius: 50%;
 
     }
+
+    img.pequeña{
+  width: 50px; height: 50px;
+  background-color: teal;
+  color: red;
+}
+img.mediana{
+  width: 100px; height: 100px;
+}
+img.grande{
+  width: 200px; height: 200px;
+}
+
+   
 </style>
 
 
 <div class="container-fluid" >
     <div class="row">
+
         <div class="col-md-6" >
             <nav class="navbar navbar-default;" style="background-color: white">
-                <img src="" align="right" style="background-position: center; " >
+ 
+                <img src="https://media.aws.alkosto.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/3/2/32tl48_1_1.jpg" align="right" style="background-position: center; size: width: 100px; height: 100px;" >
             </nav>
 
         </div>
@@ -48,20 +60,15 @@
             <nav class="navbar navbar-default;" style="background-color: white;">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="?c=index&m=index" style="color: #e89b43; font-size: 14px">!BIENVENIDOS¡</a>
-                    <a class="navbar-brand" href="?c=index&m=nuestroCafe" style="color: #111; font-size: 14px" >NUESTRO CAFÉ</a>
-                    <a class="navbar-brand" href="?c=index&m=productos" style="color: #111; font-size: 14px">PRODUCTOS</a>
-                    <a class="navbar-brand" href="?c=index&m=nuestroCompromiso" style="color: #111; font-size: 14px">NUESTRO COMPROMISO</a>
-                    <a class="navbar-brand" href="?c=index&m=visitanos" style="color: #111; font-size: 14px">VISITANOS</a>
+                    <a class="navbar-brand" href="?c=index&m=nuestroCafe" style="color: #111; font-size: 14px" >GENERO</a>
+                    <a class="navbar-brand" href="?c=index&m=productos" style="color: #111; font-size: 14px">GENERO</a>
+                    <a class="navbar-brand" href="?c=index&m=nuestroCompromiso" style="color: #111; font-size: 14px">GENERO </a>
+                    <a class="navbar-brand" href="?c=index&m=visitanos" style="color: #111; font-size: 14px">GENERO</a>
                 </div>
-                <div class="con">
-                    <div class="element">
-                        <img src="assets/img/pepe.png" class="img-responsive"  width="50" height="50" >
-                    </div>
-                    <div class="element">
-                        <img src="assets/img/social.png" class="img-responsive"  width="50"  height="40">
-                    </div>
+                
                 </div>
             </nav>
         </div>
+
     </div>
-</div>
+
--- view/index.php
+++ view/index.php
@@ -1,26 +1,137 @@
-<section>
     <?php
-    $mostrarpelURL = 'http://api.tvmaze.com/shows';
+        $mostrarpelURL = 'http://api.tvmaze.com/shows';
+        $mostrarpelJSON= file_get_contents($mostrarpelURL);
+        $mostrarpels=json_decode($mostrarpelJSON,true);
 
-     $mostrarpelJSON= file_get_contents($mostrarpelURL);
-
-    $mostrarpel=json_decode($mostrarpelJSON,true);
-    
-    var_dump($mostrarpel);
-
-
-
-
-
-
-    /*cod para llamar una variable en un arreglo con sus datos
-    $peli = array("mostrarpel");
-    $resultado = compact("",  $peli);
-    print_r($resultado);*/
-
+       //print_r($mostrarpels);
 
     ?>
 
-<h1>MOSTRAR PELICULAS </h1>
+<h1 class="title"  style="background-color: red">MOSTRAR PELICULAS - CARRUSEL IMG  </h1>
 
-</section>
+
+
+    
+
+<!--<section class="" style="background-color: blue"  >
+    <h2>Comedia</h2>
+
+    <div class="card" style="width: 18rem;">
+        <div class="swiper-container">
+            <div class="swiper-wrapper" style="background-color: red">
+
+                <?php            
+                   foreach ($mostrarpels as $movie) {
+
+                    echo '<div class="swiper-slide"   >
+
+                        <a href="m.php?id='.$movie['id'].'">
+
+                        <img class="card-img-top" src="https://todoimagenes.co/wp-content/uploads/2019/01/165c10e635869ada45be8d76ac7e9a7b.jpg">
+
+                        <div class="card-body">
+                        <h3 class="hometitle">'.$movie['name'].'</h3></a>        
+                        </div>
+
+
+                        
+                        </div> ';                
+                    }
+                ?>
+             </div>           
+        </div>       
+    </div>
+</section>-->
+
+   <!--<div class="row">
+        <div class="col-sm-12">                 
+                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img class="d-block w-100" src="https://image.freepik.com/foto-gratis/hrc-tigre-siberiano-2-jpg_21253111.jpg" alt="First slide" width="620" height="450px">
+                          <h1>TEMA 1</h1>
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" src="https://i.pinimg.com/280x280_RS/5e/eb/9d/5eeb9d819342430dba2e833c1d4c254b.jpg" alt="Second slide" width="620" height="450px">
+                          <H1>TEMA2</H1>
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" src="https://tinypng.com/images/social/website.jpg" alt="Third slide" width="620" height="450px">
+                          <H1>TEMA 3</H1>
+                        </div>
+                     </div>
+
+
+                      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                </div>        
+        </div>
+    </div>    
+</div>-->
+
+    
+                        
+
+    <?php
+     foreach ($mostrarpels as $movie) {
+     echo '<div class="carousel-item active">                
+                <img class="d-block w-100" src="http://api.tvmaze.com/shows/1/images'.$movie['id'].'width="620" height="450px">
+                <a href="m.php?id='.$movie['id'].'"></a>
+                <h1>'.$movie['name'].'</h1><br>
+                <h1>'.$movie['status'].'</h1><br>
+                <h1>'.$movie['language'].'</h1>
+                
+            </div>';
+                 }
+                ?>
+
+                        
+
+
+<!--<div class="container">
+
+    <section class="panel">
+    <h2>comedia</h2>
+    <div class="topslider">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+
+                <?php            
+                   foreach ($mostrarpels as $movie) {
+
+                    echo '<div class="swiper-slide">
+
+                        <a href="m.php?id='.$movie['id'].'">
+
+                        <img style=" width: 300px; height: 400px;" src="https://todoimagenes.co/wp-content/uploads/2019/01/165c10e635869ada45be8d76ac7e9a7b.jpg">
+
+                        <div class="card-body">
+                        <h3 class="hometitle">'.$movie['name'].'</h3></a>        
+                        </div>
+
+
+                        
+                        </div> ';                
+                    }
+                 ?>
+                
+
+            </div>
+            
+        </div>
+        
+    </div>
+    </section>
+
+    <section>
+        <h1>otra seccion</h1>
+    </section>
+    
+</div>-->
